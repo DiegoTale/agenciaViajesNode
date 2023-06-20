2754bde0b562a874021d5772405e7f62aec53549
Diego Tale <dperezt1@miumg.edu.gt>
2023-06-20 13:18:22 -0600

    primer commit\

---
+++ models/Testimoniales.js
@@ -0,0 +1,21 @@
+import Sequelize from 'sequelize';
+import db from '../config/db.js';
+
+export const Testimonial = db.define('testimoniales', {
+    nombre: {
+        type: Sequelize.STRING
+    },
+    correo: {
+        type: Sequelize.STRING
+    },
+    mensaje: {
+        type: Sequelize.DATE
+    }
+},
+
+{
+    timestamps: false,
+    createdAt: 'fecha_creacion', 
+    updatedAt: 'fecha_actualizacion',
+    db
+});
